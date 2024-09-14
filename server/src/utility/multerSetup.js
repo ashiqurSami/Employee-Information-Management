1d8248d00955b95b9a4364a3f6afe19597c34f6c
ashiqursami <2020741@iub.edu.bd>
2024-09-14 06:10:21 +0600

    fix: add employee

--- server/src/utility/multerSetup.js
+++ server/src/utility/multerSetup.js
@@ -1,15 +1,25 @@
 // Multer setup for image uploading
+const fs = require("fs");
 const multer = require("multer");
+const path = require("path");
 
 exports.multerSetup = () => {
+  const uploadDir = path.join(__dirname, "../uploads");
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true }); // Create the 'uploads' directory if it doesn't exist
+  }
+
+  // Multer setup for image uploading
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "uploads/");
+      cb(null, uploadDir); // Use the dynamically created 'uploads' directory
     },
     filename: function (req, file, cb) {
-      cb(null, Date.now() + "-" + file.originalname);
+      cb(null, Date.now() + "-" + file.originalname); // Timestamp to make filenames unique
     },
   });
   const upload = multer({ storage });
-  return upload
+  return upload;
 };
+
+// Ensure 'uploads' directory exists
